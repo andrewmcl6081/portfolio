92e87caf9881db115d6a37a07c79fc60f814cebe
Andrew McLaughlin <andrewmcl6081@gmail.com>
2025-01-08 16:48:09 -0600

    Refactored Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import "@/data/toolsData";
+import toolsData from "@/data/toolsData";
 
 const Skills = () => {
   return (
@@ -9,198 +11,24 @@ const Skills = () => {
         </p>
         <h2 className="py-4">What I Can Do</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
-                  alt="Python Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                Python
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-                  alt="Javascript Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                JavaScript
+          {toolsData.map((tool, index) => (
+            <div key={index} className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image
+                    src={tool.src}
+                    alt={tool.alt}
+                    height={60}
+                    width={60}
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  {tool.displayName}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
-                  alt="Java Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                Java
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
-                  alt="CPlusPlus Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                C/C++
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg"
-                  alt="Docker Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                Docker
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
-                  alt="AWS Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                AWS
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
-                  alt="MySQL Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                SQL
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
-                  alt="MongoDB Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                MongoDB
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-                  alt="React Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                React
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/opencv/opencv-original.svg"
-                  alt="OpenCV Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                Computer Vision
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
-                  alt="Express Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                Express
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-lg shadow-[#9a4ce7] rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
-                  alt="Git Icon"
-                  height={60}
-                  width={60}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                Git
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/data/toolsData.js
@@ -0,0 +1,65 @@
+
+const toolsData = [
+  { 
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg", 
+    alt: "Python Icon",
+    displayName: "Python",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", 
+    alt: "Javascript Icon",
+    displayName: "JavaScript",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+    alt: "Java Icon",
+    displayName: "Java",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+    alt: "CPlusPlus Icon",
+    displayName: "C/C++",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+    alt: "Docker Icon",
+    displayName: "Docker",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+    alt: "AWS Icon",
+    displayName: "AWS",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
+    alt: "MySQL Icon",
+    displayName: "SQL",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
+    alt: "MongoDB Icon",
+    displayName: "MongoDB",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    alt: "React Icon",
+    displayName: "React",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/opencv/opencv-original.svg",
+    alt: "OpenCV Icon",
+    displayName: "Computer Vision",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
+    alt: "Express Icon",
+    displayName: "Express",
+  },
+  {
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+    alt: "Git Icon",
+    displayName: "Git",
+  }
+];
+
+export default toolsData;
