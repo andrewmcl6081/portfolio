0ff2e49f5f90deec7a09fbb849f604105c291c56
DESKTOP-UCETDSH\andre <andrewmcl6081@gmail.com>
2025-01-15 16:31:30 -0600

    Began adding new Projects component

---
+++ components/Projects.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+
+const Projects = () => {
+  return (
+  )
+}
---
+++ data/projectsData.js
@@ -0,0 +1,17 @@
+
+import { MessageCircleMore } from "lucide-react";
+
+const projects = [
+  {
+    title: "Cloud Chat",
+    description: "A highly scalable real-time mesaaging platform supporting multi-user conversations, file attachments, user lookup by email, presence tracking, and powered by AWS",
+    technologies: ["Remix, WebSockets, Auth0, Redis, RDS"],
+    github: "https://github.com/andrewmcl6081/cloudchat",
+    category: "fullstack",
+    gradientFrom: "from-blue-500",
+    gradientTo: "to-purple-600",
+    icon: MessageCircleMore
+  }
+];
+
+export default projects;
